a6b290c9dabf43da2ea2858e3cc34470a7d738d0
OlaBekkevold <ola.k.bekkevold@gmail.com>
2023-09-30 13:35:54 +0200

    Create a design for a calculator

---
+++ script.js
@@ -0,0 +1,62 @@
+let 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let operator = prompt("Which operation do you want to use. +, -, * or /");
+
+if (operator === "+" || operator === "-" || operator === "*" || operator === "/") {
+
+    let num1 = parseInt(prompt("Enter your first number"));
+    let num2 = parseInt(prompt("Enter your second number"));
+    if(isNaN(num1) || isNaN(num2)) {
+        alert("Please enter a number")
+    } else {
+        switch (operator) {
+            case "+":
+                alert(`${num1} ${operator} ${num2} = ` + (num1 + num2));
+                break;
+            case "-":
+                alert(`${num1} ${operator} ${num2} = ` + (num1 - num2));
+                break;
+            case "*":
+                alert(`${num1} ${operator} ${num2} = ` + (num1 * num2));
+                break;
+            case "/":
+                alert(`${num1} ${operator} ${num2} = ` + (num1 / num2));
+                break;
+            default:
+                alert("Invalid input. Please reload the page.")
+    
+        }
+
+
+    }
+
+    
+} else {
+    alert("Invalid operator")
+
+}*/
+
+
+
+
+
+
+  
+
+
